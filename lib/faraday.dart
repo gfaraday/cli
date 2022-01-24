a277bedf403ff12b53ae5200ba9ffe0add55fb1a
run <a936381813@163.com>
2022-01-24 16:31:42 +0800

    refactor: version support

--- lib/faraday.dart
+++ lib/faraday.dart
@@ -11,6 +11,8 @@ const routeAnnotation = ['entry'];
 
 final log = Logger('faraday');
 
+const versions = ['--version', 'version', '-v'];
+
 void main(List<String> arguments) {
   final verbose = arguments.contains('--verbose');
 
@@ -18,7 +20,17 @@ void main(List<String> arguments) {
   Logger.root.onRecord.listen(recordAnsiLog);
   Logger.root.level = verbose ? Level.ALL : Level.CONFIG;
 
-  if (arguments.length == 1 && arguments.first == '--version') {
+  if (arguments.length == 1 &&
+      versions.contains(arguments.first.toLowerCase())) {
+    final welcome = '''
+   ___                   _             
+  / __\\_ _ _ __ __ _  __| | __ _ _   _ 
+ / _\\/ _` | '__/ _` |/ _` |/ _` | | | |
+/ / | (_| | | | (_| | (_| | (_| | |_| |
+\\/   \\__,_|_|  \\__,_|\\__,_|\\__,_|\\__, |
+                                 |___/       
+    ''';
+    log.info(welcome);
     print(version);
     return;
   }
